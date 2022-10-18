cc365a54fac0814907cb32535a79962b84792cba
polodeh <mice.eyes.228@mail.ru>
2022-10-18 11:27:31 +0400

    courier

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import math
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     def __str__(self):
         return f'Point with coords {self.x}, {self.y}'
+    def get_dist(self, other_point):
+        x1 = self.x
+        x2 = other_point.x
+        y1 = self.y
+        y2 = other_point.y
+        dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        return dist
+
 p1 = Point(1, 2)
-print(p1)
+p2 = Point(1, 0)
+dist = p1.get_dist(p2)
+print(dist)
 
